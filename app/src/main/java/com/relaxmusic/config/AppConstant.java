f6ef1627fdf61e2ad1af7d49fe8a1703a57f07fb
andresmarino87 <andresmarino87@gmail.com>
2016-12-13 09:32:15 -0400

    code improvments

---
+++ app/src/main/java/com/relaxmusic/config/AppConstant.java
@@ -0,0 +1,24 @@
+package com.relaxmusic.config;
+
+/*
+ * Author: Andrés Mariño
+ * Date: 05 - 02 - 2015
+ * 
+ * */
+
+
+import com.relaxmusic.R;
+
+public final class AppConstant {
+	private AppConstant(){}
+
+	final static public String AppName="RelaxMusic";
+	final static public String ImagesBaseDir= "Images/";
+	final static public String ImagesNormalDir= "Images/Sounds/normal/";
+	final static public String ImagesSelectedDir= "Images/Sounds/selected/";
+	final static public String ImagesSoundsDir= "SoundsAndroid/";
+	final static public int MaxSoundsNumber=3;
+	final static public int [] Ids={R.id.sound1,R.id.sound2,R.id.sound3,
+		R.id.sound4,R.id.sound5,R.id.sound6,
+		R.id.sound7,R.id.sound8,R.id.sound9};	
+}
